9caf9b8de9190cbcb2fc0ca0c499d0cb59e33626
Agam <agamkoradiya333@gmail.com>
2022-07-03 13:56:28 +0530

    12. Fixed app theme issue.

--- app/src/main/java/com/codefun/saveaminutelauncher/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/codefun/saveaminutelauncher/presentation/activities/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.codefun.saveaminutelauncher.presentation.activities
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.codefun.saveaminutelauncher.R
+import com.codefun.saveaminutelauncher.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemGestures())
+            view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+
+            WindowInsetsCompat.CONSUMED
+        }
+
+    }
+
+
+    override fun getTheme(): Resources.Theme {
+        val theme = super.getTheme()
+        theme.applyStyle(R.style.App2ndTheme, true)
+        return theme
     }
 }
